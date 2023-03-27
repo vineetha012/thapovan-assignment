479e144a801e0044c3b50b8d6a5e8990889e27d1
vineetha <yenugulavineetha2001@gmail.com>
2023-03-27 11:10:32 +0530

    register route is created using bcrypt

---
+++ server/routes/rigester.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const { body, validationResult } = require('express-validator')
+const Users = require('../models/userSchema')
+
+router.use(express.json())
+
+router.post('/',
+    body('email').isEmail()
+    , async (req, res) => {
+        try {
+            console.log(req.body);
+            const errors=validationResult(req)
+            if(!errors.isEmpty){
+                return res.status(400).json({
+                    errors: errors.array()
+                })
+            }
+            const {name,email,password}=req.body
+            let user_exist=await Users.findOne({email:email})
+            if(user_exist){
+                return res.status(400).json({
+                    status:'registration Failed',
+                    message:'Email is Already Existed'
+                })
+            }
+            bcrypt.hash(password,10,async function(err, hash){
+                if(err){
+                    return res.status(500).json({
+                        error:err.message
+                    })
+                }
+                if(hash){
+                    let newUser=await Users.create({
+                        name,
+                        email,
+                        password:hash
+                    })
+                }
+                return res.status(201).json({
+                    status: "Success",
+                    message: "User registered successfully"
+                })
+            })
+        } catch (error) {
+            return res.status(500).json({
+                error: error.message
+            })
+        }
+    }
+)
+module.exports = router
